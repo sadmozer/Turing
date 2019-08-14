4e1f280419adc2a3df0cd0007c665e04531a082d
sadmozer <n.cardelli2@outlook.it>
2019-08-14 03:29:39 +0200

    Modifiche minori

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,6 @@
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.ExportException;
-import java.rmi.server.UnicastRemoteObject;
-
 
 public class Server {
     public static void main(String[] args) {
